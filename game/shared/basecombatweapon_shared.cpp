6b11863c10a72a51915afd53453b97919fd56e90
PiMoNFeeD <zarod2005@yandex.ru>
2021-08-01 03:03:18 +0300

    how could I forget to send weapon world model from server to client??

--- game/shared/basecombatweapon_shared.cpp
+++ game/shared/basecombatweapon_shared.cpp
@@ -3462,6 +3462,7 @@ BEGIN_NETWORK_TABLE(CBaseCombatWeapon, DT_BaseCombatWeapon)
 
 	SendPropInt( SENDINFO( m_iPrimaryReserveAmmoCount ), 10),
 	SendPropInt( SENDINFO( m_iSecondaryReserveAmmoCount ), 10 ),
+	SendPropEHandle( SENDINFO(m_hWeaponWorldModel) ),
 	SendPropInt( SENDINFO( m_iNumEmptyAttacks ), 8 ),
 #else
 	RecvPropDataTable("LocalWeaponData", 0, 0, &REFERENCE_RECV_TABLE(DT_LocalWeaponData)),
@@ -3473,6 +3474,7 @@ BEGIN_NETWORK_TABLE(CBaseCombatWeapon, DT_BaseCombatWeapon)
 	RecvPropEHandle( RECVINFO(m_hOwner ) ),
 	RecvPropInt( RECVINFO( m_iPrimaryReserveAmmoCount)),
 	RecvPropInt( RECVINFO( m_iSecondaryReserveAmmoCount)),
+	RecvPropEHandle( RECVINFO(m_hWeaponWorldModel) ),
 	RecvPropInt( RECVINFO( m_iNumEmptyAttacks )),
 #endif
 END_NETWORK_TABLE()
