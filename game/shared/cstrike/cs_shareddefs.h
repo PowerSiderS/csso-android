1f1e3769b76ca70c9048c22320da228406f83d68
modniykrokyy <yakovlev1986danyal@gmail.com>
2024-09-14 11:06:57 +0300

    old leet gloves

--- game/shared/cstrike/cs_shareddefs.h
+++ game/shared/cstrike/cs_shareddefs.h
@@ -210,7 +210,7 @@ extern CUtlVectorInitialized< const char* > KnivesEntities;
 //--------------
 #define DMG_HEADSHOT		(DMG_LASTGENERICFLAG<<1)
 
-#define MAX_GLOVES 17
+#define MAX_GLOVES 19
 struct PlayerGloves
 {
 	const char*	szViewModel;
