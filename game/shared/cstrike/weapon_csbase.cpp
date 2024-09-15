cf4d7b2b7647a1b1d0a029f6fdd4ceb8d1ee3152
DMK95 <maga200544@gmail.com>
2024-09-15 17:46:57 +0300

    ACT_VM_EMPTY_DRAW

--- game/shared/cstrike/weapon_csbase.cpp
+++ game/shared/cstrike/weapon_csbase.cpp
@@ -1383,8 +1383,17 @@ void CWeaponCSBase::Precache( void )
 
 Activity CWeaponCSBase::GetDeployActivity( void )
 {
+	CBaseCombatCharacter *pOwner = GetOwner();
+	if (pOwner)
+	{
+		if ( GetReserveAmmoCount( AMMO_POSITION_PRIMARY ) <= 0 && LookupActivity( "ACT_VM_EMPTY_DRAW" ) > 0 )
+		{
+			return ACT_VM_EMPTY_DRAW;
+		}
+	}
 	return ACT_VM_DRAW;
 }
+
 
 bool CWeaponCSBase::DefaultDeploy( char *szViewModel, char *szWeaponModel, int iActivity, char *szAnimExt )
 {
