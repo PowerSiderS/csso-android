734c9ce4f7d804b59e75572484ca63708b530732
denz8028 <denzel8028@hotmail.com>
2024-09-16 16:00:30 +0300

    CSGO spawnpoints code

--- game/server/cstrike/cs_player.h
+++ game/server/cstrike/cs_player.h
@@ -914,7 +914,7 @@ protected:
 
 	bool RunMimicCommand( CUserCmd& cmd );
 
-	bool SelectSpawnSpot( const char *pEntClassName, CBaseEntity* &pSpot );
+	bool SelectSpawnSpot( const char *pEntClassName, CBaseEntity* &pStartSpot );
 
 	void SetModelFromClass( void );
 	void SetRandomClassSkin( void );
