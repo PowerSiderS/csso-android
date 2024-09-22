48158f02547656cb8408af727e5b0215346693f3
DMK95 <maga200544@gmail.com>
2024-09-22 19:30:03 +0300

    this shit fix animstate

--- game/shared/basecombatweapon_shared.cpp
+++ game/shared/basecombatweapon_shared.cpp
@@ -645,6 +645,8 @@ void CBaseCombatWeapon::Spawn( void )
 	// been hand-placed by level designers. We only want to remove
 	// weapons that have been dropped by NPC's.
 	SetRemoveable( false );
+
+	CreateWeaponWorldModel();
 #endif
 
 	// Bloat the box for player pickup
@@ -1928,6 +1930,12 @@ selects and deploys each weapon as you pass it. (sjb)
 bool CBaseCombatWeapon::Deploy( )
 {
 	MDLCACHE_CRITICAL_SECTION();
+
+#if !defined( CLIENT_DLL )
+	CreateWeaponWorldModel();
+	ShowWeaponWorldModel( false ); // don't show right away, wait for the deploy anim to unhide it
+#endif
+
 	bool bResult = DefaultDeploy( (char*)GetViewModel(), (char*)GetWorldModel(), GetDrawActivity(), (char*)GetAnimPrefix() );
 
 	// override pose parameters
@@ -1955,6 +1963,10 @@ Activity CBaseCombatWeapon::GetDrawActivity( void )
 bool CBaseCombatWeapon::Holster( CBaseCombatWeapon *pSwitchingTo )
 { 
 	MDLCACHE_CRITICAL_SECTION();
+
+#if !defined( CLIENT_DLL )
+	ShowWeaponWorldModel( false );
+#endif
 
 	// cancel any reload in progress.
 	m_bInReload = false; 
