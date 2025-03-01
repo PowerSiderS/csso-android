55f87ee398991e2d4e43dbca3bdfec62c604249a
modniykrokyy <yakovlev1986danyal@gmail.com>
2025-03-01 20:20:52 +0300

    dont add world gloves model if selected gloves are the same as player model gloves

--- game/client/cstrike/clientmode_csnormal.cpp
+++ game/client/cstrike/clientmode_csnormal.cpp
@@ -1103,8 +1103,6 @@ void UpdateClassImageEntity(
 
 	C_BaseAnimating *pPlayerModel = g_ClassImagePlayer.Get();
 
-	bool bCreateGloves = false;
-
 	// Does the entity even exist yet?
 	bool recreatePlayer = ShouldRecreateImageEntity( pPlayerModel, pModelName );
 	if ( recreatePlayer )
@@ -1120,10 +1118,15 @@ void UpdateClassImageEntity(
 		g_ClassImagePlayer = pPlayerModel;
 	}
 
-	if ( pPlayerModel && pPlayerModel->DoesModelSupportGloves() )
+	bool bCreateGloves = false;
+	const char *szGlovesViewModel = NULL;
+	if ( CSLoadout()->HasGlovesSet( pLocalPlayer, pLocalPlayer->GetTeamNumber() ) )
 	{
-		if ( CSLoadout()->HasGlovesSet( pLocalPlayer, iTeamNumber ) )
-			bCreateGloves = true;
+		szGlovesViewModel = GetGlovesInfo( CSLoadout()->GetGlovesForPlayer( pLocalPlayer, pLocalPlayer->GetTeamNumber() ) )->szViewModel;
+	}
+	if ( pPlayerModel && szGlovesViewModel && pLocalPlayer->m_szPlayerDefaultGloves && pPlayerModel->DoesModelSupportGloves( szGlovesViewModel, pLocalPlayer->m_szPlayerDefaultGloves ) )
+	{
+		bCreateGloves = true;
 	}
 
 	C_BaseAnimating *pWeaponModel = g_ClassImageWeapon.Get();
@@ -1376,8 +1379,6 @@ void UpdateBuyMenuImageEntity(
 
 	C_BaseAnimating *pPlayerModel = g_BuyMenuImagePlayer.Get();
 
-	bool bCreateGloves = false;
-
 	// Does the entity even exist yet?
 	bool recreatePlayer = ShouldRecreateImageEntity( pPlayerModel, modelinfo->GetModelName( pLocalPlayer->GetModel() ) );
 	if ( recreatePlayer )
@@ -1393,10 +1394,15 @@ void UpdateBuyMenuImageEntity(
 		g_BuyMenuImagePlayer = pPlayerModel;
 	}
 
-	if ( pPlayerModel && pPlayerModel->DoesModelSupportGloves() )
+	bool bCreateGloves = false;
+	const char *szGlovesViewModel = NULL;
+	if ( CSLoadout()->HasGlovesSet( pLocalPlayer, pLocalPlayer->GetTeamNumber() ) )
 	{
-		if ( CSLoadout()->HasGlovesSet( pLocalPlayer, pLocalPlayer->GetTeamNumber() ) )
-			bCreateGloves = true;
+		szGlovesViewModel = GetGlovesInfo( CSLoadout()->GetGlovesForPlayer( pLocalPlayer, pLocalPlayer->GetTeamNumber() ) )->szViewModel;
+	}
+	if ( pPlayerModel && szGlovesViewModel && pLocalPlayer->m_szPlayerDefaultGloves && pPlayerModel->DoesModelSupportGloves( szGlovesViewModel, pLocalPlayer->m_szPlayerDefaultGloves ) )
+	{
+		bCreateGloves = true;
 	}
 
 	C_BaseAnimating *pWeaponModel = g_BuyMenuImageWeapon.Get();
