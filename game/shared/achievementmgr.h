fed9c56c7290906c627a4409f0231b76b570c064
PowerSiderS.X DARK (KiLiDARK) <nikoladark87@gmail.com>
2026-02-23 13:06:13 +0100

    Add functions to save and load achievements from file

--- game/shared/achievementmgr.h
+++ game/shared/achievementmgr.h
@@ -68,6 +68,8 @@ public:
 	void SaveGlobalState( bool bAsync = false );
 	void LoadGlobalState();
 	void SaveGlobalStateIfDirty( bool bAsync = false );
+    void SaveAchievementsToFile();
+	void LoadAchievementsFromFile();
 	bool IsAchievementAllowedInGame( int iAchievementID );
 	void EnsureGlobalStateLoaded();
 	void AwardAchievement( int iAchievementID );
