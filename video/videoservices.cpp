67cd833787db5fcbd9a7e95514f4a8cd9836f781
FJH_03 <fengjiahe5@gmail.com>
2026-05-23 13:23:34 +0800

    fix compile error and remove video_bink for win32

--- video/videoservices.cpp
+++ video/videoservices.cpp
@@ -81,7 +81,9 @@ struct VideoSystemInfo_t
 static VideoSystemInfo_t s_VideoAppSystems[] = 
 {
 	{ VideoSystem::QUICKTIME,	PLATFORM_WIN32 | PLATFORM_OSX,							"video_quicktime",  VIDEO_SUBSYSTEM_INTERFACE_VERSION },
-	{ VideoSystem::BINK,		PLATFORM_WIN32 | PLATFORM_OSX | PLATFORM_XBOX_360 | PLATFORM_LINUX,		"video_bink",	    VIDEO_SUBSYSTEM_INTERFACE_VERSION },
+#if !defined( WIN32 )
+	{ VideoSystem::BINK,		PLATFORM_OSX | PLATFORM_XBOX_360 | PLATFORM_LINUX,		"video_bink",	    VIDEO_SUBSYSTEM_INTERFACE_VERSION },
+#endif
 	//{ VideoSystem::AVI,			PLATFORM_WIN32,											"avi",				VIDEO_SUBSYSTEM_INTERFACE_VERSION },
 	//{ VideoSystem::WMV,			PLATFORM_WIN32,											"wmv",				VIDEO_SUBSYSTEM_INTERFACE_VERSION },
 	{ VideoSystem::WEBM,	PLATFORM_LINUX,							"video_webm",  VIDEO_SUBSYSTEM_INTERFACE_VERSION },
