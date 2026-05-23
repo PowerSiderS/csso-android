7956a7f80e921de8140b0837d3f7b435e20bff77
FJH_03 <fengjiahe5@gmail.com>
2026-05-23 13:35:41 +0800

    fix android build

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -2,7 +2,7 @@
 export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 ./waf configure -T release --build-game=cstrike --prefix=Srceng-mod-launcher/android --android=aarch64,host,21 --target=../aarch64 --enable-opus --togles --disable-warns &&
-./waf install --target=client,server,engine,GameUI,vgui2,vguimatsurface,vstdlib --strip
+./waf install --target=client,server,engine,vgui2,vguimatsurface,vstdlib --strip
 
 if [ -e "Srceng-mod-launcher/android/lib/arm64-v8a/README.md" ]; then
 	rm Srceng-mod-launcher/android/lib/arm64-v8a/README.md
